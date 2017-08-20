af0d08475263bd07ffdae7e157a1be8b2adf7f48
Jonn Spencer <jswiz01@gmail.com>
2017-08-20 14:04:59 -0400

    Add MacosNetplayApp project

---
+++ macos/MacosNetplayApp/MacosNetplayApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacosNetplayApp
+//
+//  Created by Jonn Spencer on 8/20/17.
+//  Copyright © 2017 Water Works. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos/MacosNetplayApp/MacosNetplayApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacosNetplayApp
+//
+//  Created by Jonn Spencer on 8/20/17.
+//  Copyright © 2017 Water Works. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
