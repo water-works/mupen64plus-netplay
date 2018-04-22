eca26fba30b612f5f4095f85b1bc2687b6b8c7f5
Jonn Spencer <jonn@jonnjonnjonn.com>
2018-04-22 18:37:35 -0400

    Update formatting to remove unnecessary diffs

--- macos/netplay/NetplayAppUITests/NetplayAppUITests.swift
+++ macos/netplay/NetplayAppUITests/NetplayAppUITests.swift
@@ -1,17 +1,10 @@
-//
-//  NetplayAppUITests.swift
-//  NetplayAppUITests
-//
-//  Created by Jonn Spencer on 4/22/18.
-//
-
 import XCTest
 
 class NetplayAppUITests: XCTestCase {
-        
+
     override func setUp() {
         super.setUp()
-        
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -21,15 +14,15 @@ class NetplayAppUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
 }
